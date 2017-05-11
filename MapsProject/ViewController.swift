dfb812cdb33b94e01403d32db9acde72be92fb2e
Miguel Tepale <miguel.tepale@gmail.com>
2017-05-11 13:03:30 -0400

    Added pin

--- MapsProject/ViewController.swift
+++ MapsProject/ViewController.swift
@@ -13,6 +13,7 @@ import MapKit
 class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
     let locationManager = CLLocationManager()
+    let annotation = MKPointAnnotation()
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var tttImageView: UIImageView!
     @IBOutlet weak var mapSegmentedObject: UISegmentedControl!
@@ -21,12 +22,14 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         super.viewDidLoad()
         
         mapView.mapType = .standard
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 40.7084257, longitude: -74.0148711)
         
         locationManager.requestWhenInUseAuthorization()
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         
         mapView.showsUserLocation = true
+        mapView.addAnnotation(annotation)
     }
 
     @IBAction func mapSegmentedAction(_ sender: UISegmentedControl) {
